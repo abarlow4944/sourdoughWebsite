9227074ca5efee134de92074518f00a573ec9d0c
abarlow4944 <192002399+abarlow4944@users.noreply.github.com>
2024-12-23 10:50:26 -0500

    Created files for header and navigation bar so that the code for it can be reused for every page of the website.

---
+++ scripts/loadHeader.js
@@ -0,0 +1,12 @@
+// for loading header and navigation bar onto every page
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetch('header.html') // retrieve header file
+        .then(response => response.text()) // convert the response into plain text
+        .then(data => { //put the data into headerPlaceholder
+            document.getElementById('headerPlaceholder').innerHTML = data;
+            document.body.classList.remove('hidden'); // show the rest of the content
+        })
+        .catch(error => console.error('Error loading header:', error)); // in case there is an error
+});
