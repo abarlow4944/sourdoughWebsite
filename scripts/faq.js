ae75b26304a7affa8559007973a5a940b79ad556
abarlow4944 <192002399+abarlow4944@users.noreply.github.com>
2024-12-30 16:40:56 -0500

    Finished Sourdough Starter portion of FAQ.

---
+++ scripts/faq.js
@@ -0,0 +1,28 @@
+// for the accordion FAQ 
+
+document.addEventListener("DOMContentLoaded", () => {
+    const menuItems = document.querySelectorAll(".faqMenu li"); //the various menu topics
+    const faqSections = document.querySelectorAll(".faqSection"); //the faq sections for each menu topic
+
+    menuItems.forEach(menuItem => {
+        menuItem.addEventListener("click", () => { //if a menu item is clicked
+            faqSections.forEach(section => section.classList.remove("active")); //hide each faq section
+            
+            const topic = menuItem.dataset.topic; //get the data-topic value of the selected menuItem
+            document.getElementById(topic).classList.add("active"); //show the faqContent of the selected menuItem
+        });
+    });
+
+
+    const accordions = document.querySelectorAll(".accordion");
+    
+    accordions.forEach(accordion => {
+        accordion.addEventListener("click", () => {
+            const panel = accordion.nextElementSibling;
+            const arrow = accordion.querySelector(".arrow");
+
+            panel.classList.toggle("active"); //show/remove answer when clicked
+            arrow.classList.toggle("rotate"); 
+        });
+    });
+});
