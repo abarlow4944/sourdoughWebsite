c638a95209e6ac710e8d36481746e55f428afc4e
abarlow4944 <192002399+abarlow4944@users.noreply.github.com>
2024-12-30 11:51:26 -0500

    Made functioning accordion FAQ.

--- scripts/faqAccordion.js
+++ scripts/faqAccordion.js
@@ -1,9 +1,16 @@
 // for the accordion FAQ 
 
-document.addEventListener("DOMContentLoaded", function() {
-    const accordion = document.querySelector(".accordion");
+document.addEventListener("DOMContentLoaded", () => {
+    const accordion = document.querySelectorAll(".accordion");
     
+    accordion.forEach(accordion => {
+        accordion.addEventListener("click", () => {
+            const panel = accordion.nextElementSibling;
+            const arrow = accordion.querySelector(".arrow");
 
-
-
-}
+            panel.classList.toggle("active"); //show/remove answer when clicked
+            arrow.classList.toggle("rotate");
+            
+        })
+    });
+});
